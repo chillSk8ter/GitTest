b439f508a76569bcbf2e874342bfeb1ada8cbd94
cherrynode <cherrysk8ter@gmail.com>
2021-11-25 22:24:36 +0800

    h

--- src/test1.java
+++ src/test1.java
@@ -15,6 +15,7 @@ public class test1 {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+
         list.add(3);
         list.remove(new Integer(1));
         System.out.println(list.toString());
